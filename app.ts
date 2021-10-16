2b52558327780952144f3a0a5cefd98369bbd9e0
Tobi Ajibade <tobiajibade2017@yandex.com>
2021-10-16 13:44:11 +0100

    Wrote server setup code for typescript, deleted js files

---
+++ app.ts
@@ -0,0 +1,29 @@
+import { config  } from 'dotenv';
+if(process.env.NODE_ENV !== "production") {
+    config();
+}
+import express, { NextFunction, Request, Response } from 'express';
+import exphbs from 'express-handlebars';
+import path from 'path';
+import { indexRouter } from './routes/index';
+
+const app = express();
+
+app.use(express.urlencoded({ extended : false }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("hbs", exphbs({defaultLayout: "main", extname: "hbs"}));
+app.set("view engine", "hbs");
+
+app.use("/", indexRouter);
+app.use('/b', () => {});
+app.use((req:Request, res:Response, next: NextFunction) => {
+    res.render("pages/404", { page_name : "404 - Not found"});
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
--- routes/index/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { index } = require("../../controller/index/indexController");
-
-router.get('/', index);
-
-module.exports = router;
